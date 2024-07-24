e65c508334262fc4ce48b0915ab0342d4a3b6530
davidmuo <d.muotohfra@alustudent.com>
2024-07-24 22:04:53 +0100

    Update student.py

--- grade_book_app/student.py
+++ grade_book_app/student.py
@@ -1,21 +1,35 @@
+# student.py
+
+from course import Course
+
 class Student:
-    def __init__(self, email, names, courses_registered=None, GPA=0.0):
+    def __init__(self, email, names):
         self.email = email
         self.names = names
-        self.courses_registered = courses_registered if courses_registered else []
-        self.GPA = GPA
+        self.courses_registered = []
+        self.GPA = 0.0
 
     def calculate_GPA(self):
         if not self.courses_registered:
             self.GPA = 0.0
-        else:
-            total_credits = sum(course['course'].credits for course in self.courses_registered)
-            total_points = sum(course['grade'] * course['course'].credits for course in self.courses_registered)
-            self.GPA = total_points / total_credits if total_credits != 0 else 0.0
+            return
+        total_points = sum(course['grade'] * course['course'].credits for course in self.courses_registered)
+        total_credits = sum(course['course'].credits for course in self.courses_registered)
+        self.GPA = total_points / total_credits if total_credits > 0 else 0.0
 
     def register_for_course(self, course, grade=0.0):
         self.courses_registered.append({'course': course, 'grade': grade})
         self.calculate_GPA()
+
+    @classmethod
+    def from_dict(cls, data):
+        student = cls(data['email'], data['names'])
+        student.courses_registered = [
+            {'course': Course.from_dict(course['course']), 'grade': course['grade']}
+            for course in data['courses_registered']
+        ]
+        student.GPA = data['GPA']
+        return student
 
     def to_dict(self):
         return {
@@ -27,10 +41,3 @@ class Student:
             ],
             'GPA': self.GPA
         }
-
-    @classmethod
-    def from_dict(cls, data):
-        courses_registered = [
-            {'course': Course.from_dict(course['course']), 'grade': course['grade']}
-        ]
-        return cls(data['email'], data['names'], courses_registered, data['GPA'])
